1d37e7980099ed5418f751465918311914d42ad5
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-02-26 12:14:50 +0530

    Logical Question

---
+++ Aaa.java
@@ -0,0 +1,76 @@
+/*
+class Aaa {
+    public static void main(String[] args) { // The main method should always have void as its return type.
+        System.out.println("Hello Java");
+    }
+}
+*/
+
+/*
+class Aaa{
+    public static int main(String[] str) { // // The main method should always have void as its return type.
+        System.out.println("Hello Java");
+        return main(str); // the method calls itself recursively:
+    }
+}
+*/
+
+/*
+class Aaa {
+    public static void main(String[] args) {
+        double x = 25.6;
+        System.out.println(Math.ceil(x));   // Outputs 26.0 : Round Up
+        System.out.println(Math.floor(x));  // Outputs 25.0 : Round Down
+        System.out.println(Math.round(x));  // Outputs 26   : Round to nearest Integer
+    }   
+}
+*/
+
+/*
+class Aaa {
+    public static void main(String[] args) {
+        String str = ""; // String Pool (a part of the heap memory).
+        System.out.println(str.length());
+    }   
+}
+*/
+
+
+/*
+class Treee {
+    public static int ram(){
+        int x = 1;
+        return x;
+        
+    }
+    public static void main(String[] args) {
+       System.out.println("Hello Java");
+       ram();
+    }
+}
+*/
+
+/*
+class Treee {
+    public static int ram() {
+        int x = 1;
+        return x;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        int result = ram();
+        System.out.println("ram() returned: " + result);
+    }
+}
+*/
+
+/*
+import static java.lang.System.out;
+
+class Aaa{
+    public static void main(String[] args){
+        out.println("Hello Java");
+    }
+}
+*/
+
