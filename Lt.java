0667faa64bcf318c8b8530589abe1557008cc638
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-03-01 17:16:28 +0530

    list

---
+++ Lt.java
@@ -0,0 +1,40 @@
+/*
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+class Lt{
+    public static void main(String[] args) {
+        List l = new ArrayList();
+        l.add(10);
+        l.add("Rahule");
+        l.add("Raja");
+
+        ListIterator li = l.listIterator();
+        while(li.hasNext()){
+            System.out.println(li.next());
+        }
+    }
+}
+*/
+
+/*
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+class Lt{
+    public static void main(String[] args) {
+        List l = new ArrayList();
+        l.add(10);
+        l.add("Rahule");
+        l.add("Raja");
+
+        ListIterator li = l.listIterator();
+        li.next(); // cursore pahle top position me hota hai,phir dono element ke mid me aata hai phir next se 2nd element me jata hai so countinue ...
+        li.next();
+        li.next();
+        while(li.hasPrevious()){
+            System.out.println(li.previous());
+        }
+    }
+}
+*/
