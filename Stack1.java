bed37154d919d99d43e6d0fd85f12c88099cceaa
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-03-03 15:10:32 +0530

    Stack.java

---
+++ Stack1.java
@@ -0,0 +1,36 @@
+/*
+import java.util.Stack;
+class Stack1 {
+   public static void main(String[] args) {
+    Stack s = new Stack();
+    s.push("Ram");
+    s.push(100);
+    s.push(100.10);
+    s.push("Radha");
+    
+    System.out.println(s);
+    System.out.println(s.pop());
+    System.out.println(s);
+   } 
+}
+*/
+
+/*
+import java.util.Stack;
+class Stack1 {
+   public static void main(String[] args) {
+    Stack s = new Stack();
+    s.push("Ram");
+    s.push(100);
+    s.push(100.10);
+    s.push(200.5);
+
+    System.out.println(s);
+    System.out.println(s.peek()); // return top most element 
+    System.out.println(s);
+
+    System.out.println(s.search("Ram"));
+    System.out.println(s.search(100000)); // aager koi ve cheaje nahi hai to -1 return karta hai .
+   } 
+}
+*/
