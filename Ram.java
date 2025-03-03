f2e6aeaf7b81eab0000ae2e8cd79e700f41334f0
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-03-03 15:12:56 +0530

    .java

---
+++ Ram.java
@@ -0,0 +1,49 @@
+/*
+class Ram {
+    public static void main(String[] args) {
+        double x = 22.9;
+        // // Using Math.round()
+        int roundValue = Math.round((float)x);
+        System.out.println("Using Math.round " + roundValue);
+        // // Using Math.ceil()
+        int ceilValue = (int) Math.ceil(x);
+        System.out.println("Using Math.ceil: " + ceilValue);
+        // Using type casting (incorrect for rounding up)
+        int castValue = (int) x;
+        System.out.println("Using type casting " + castValue);
+    }
+}
+*/
+
+/*
+class Ram{
+    public static void main(String[] args) {
+       double x = 22.9;
+       int roundValue = Math.round((float)x);
+       System.out.println(roundValue);
+
+       double roundValue2 = Math.round(x);
+       System.out.println(roundValue2);
+    }
+}
+*/
+
+/* 
+class Ram{
+    public static void main(String[] args) {
+        double x = 25.9;
+        int ceilValue = (int) Math.ceil(x);
+        System.out.println(ceilValue);
+    }
+}
+*/
+
+/*
+class Ram{
+    public static void main(String[] args) {
+        double x = 31.5;
+        int roundValue = (int) x;
+        System.out.println(roundValue);
+    }
+}
+*/
